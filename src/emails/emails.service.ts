caa6677de7823a86b61e9bc7caf0d5c55ad1e543
Irving Dinh <59089337+irvingdinh@users.noreply.github.com>
2023-01-06 15:30:15 +0700

    Add `emails` accessing APIs (#3)

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
-import { Attributes, CreateOptions, CreationAttributes } from 'sequelize'
+import { Attributes, CreateOptions, CreationAttributes, FindOptions } from 'sequelize'
 
 import { Email } from './models/email.model'
 
@@ -16,5 +16,13 @@ export class EmailsService {
     options?: CreateOptions<Attributes<Email>>
   ): Promise<Email> {
     return this.emailModel.create<Email>(values, options)
+  }
+
+  public async findAll(options?: FindOptions<Attributes<Email>>): Promise<Email[]> {
+    return this.emailModel.findAll(options)
+  }
+
+  public async findOne(options?: FindOptions<Attributes<Email>>): Promise<Email | null> {
+    return this.emailModel.findOne(options)
   }
 }
